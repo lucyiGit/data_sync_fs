ffb65bd04d3f2d09df95db73337b7b0e1ac428dd
lucyiGit <77312428+lucyiGit@users.noreply.github.com>
2025-08-22 07:20:33 +0000

    v1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const cors = require("cors"); // 引入cors中间件
 
 const { getTableMeta } = require("./table_meta.js");
 const { getTableRecords } = require("./table_records.js");
@@ -8,13 +9,26 @@ const { judgeEncryptSignValid } = require("./request_sign.js");
 
 const app = express();
 
+// 配置CORS
+app.use(
+    cors({
+        origin: "https://4f037149-92a1-4beb-9833-d38f60cf9950-00-3olderhlk9mlz.pike.replit.dev", // 允许的前端域名
+        methods: ["GET", "POST", "OPTIONS"], // 允许的HTTP方法
+        allowedHeaders: ["Content-Type", "Authorization"], // 允许的请求头，必须包含Content-Type
+    }),
+);
+
 app.use(express.json());
+
+// 处理预检请求
+app.options("*", cors());
 
 app.get("/", (req, res) => {
     res.send("hello world");
 });
 
 app.get("/meta.json", (req, res) => {
+    console.log("meta.json的请求数据", req.headers);
     fs.readFile(
         path.join(__dirname, "./public/meta.json"),
         "utf8",
@@ -28,9 +42,7 @@ app.get("/meta.json", (req, res) => {
 app.post("/api/table_meta", (req, res) => {
     console.log("table_meta的请求数据", req.body);
     console.log("加密判断结果：", judgeEncryptSignValid(req));
-
     const result = { code: 0, message: "POST请求成功", data: getTableMeta() };
-
     res.status(200).json(result);
 });
 
